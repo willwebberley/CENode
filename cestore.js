13996b389e4dc551b6841032e96b2b3ff4aa1b03
Will Webberley <willwebberley@gmail.com>
2015-01-07 16:13:35 +0000

    trying to add instances

--- cestore.js
+++ cestore.js
@@ -1,55 +1,76 @@
-var cestore = {
-    concepts : [
+function cestore(){
+    var concepts = [
+        {name: "entity", id: 1, parent_id: null},
+        {name: "card", id: 2, parent_id: 1},
+        {name: "tell_card", id: 3, parent_id: 2}
+    ];
+    var instances = [
 
-    ],
-    instances : [
+    ];
+    var relationships = [
+        {ce: "from the individual", relationship: "from"},
+        {ce: "to the agent", relationship: "to"},
+        {ce: "named", relationship: "name"},
+        {ce: "has", relationship: null}
+    ];
 
-    ],
-    cards : [],
-    common_words : ["a"],
-    next_id : 0,
+    var cards = [];
+    var common_words = ["a"];
+    var next_id = concepts.length;
+    var card_count = cards.length;
+    var agent_name = "Moira";
+    var last_polled_timestamp = 0;
 
-    get_concept_by_name : function(name){
+    var get_concept_by_name = function(name){
         if(name == null){return null;}
-        for(var i = 0; i < cestore.concepts.length; i++){
-            if(cestore.concepts[i].name() == name.toLowerCase()){
-                return cestore.concepts[i];
+        for(var i = 0; i < concepts.length; i++){
+            if(concepts[i].name == name.toLowerCase()){
+                return concepts[i];
             }
         }
         return null;
-    },
+    }
 
-    add_concept : function(name, parent_name){
-        if(cestore.concepts.indexOf(name) == -1){
-            cestore.next_id++;
-            var parent = cestore.get_concept_by_name(parent_name);
-            if(parent_name != null && parent == null){
-                cestore.concepts.push({name: parent_name, id: cestore.next_id});
-                cestore.next_id++;
-                cestore.concepts.push({name: name, id: cestore.next_id, parent_id: cestore.next_id-1});
-            }
-            else if(parent_name != null && parent != null){
-                cestore.concepts.push({name: name, id: cestore.next_id, parent_id: parent.id});
-            }
-            else if(parent_name == null){
-                cestore.concepts.push({name: name, id: cestore.next_id, parent_id: null});
-            }
+    this.get_concepts = function(){
+        return concepts;
+    }
+
+    var add_concept = function(name, parent_name){
+        if(get_concept_by_name(name) != null){throw "Concept already exists.";}
+        var concept;
+        concept.name = name;
+        next_id++;
+        var parent = get_concept_by_name(parent_name);
+        if(parent_name != null && parent == null){
+            concepts.push({name: parent_name, id: next_id});
+            next_id++;
+            concept.id = next_id;
+            concept.parent_id = next_id-1;
         }
-    },
-
-    add_instance : function(concept, name){
-        if(cestore.concepts.indexOf(concept) > -1){
-            cestore.instances.push({});
+        else if(parent_name != null && parent != null){
+            concept.id = next_id;
+            concept.parent_id = parent.id;
         }
-    },
+        else if(parent_name == null){
+            concept.id = next_id;
+            concept.parent_id = null;
+        }
+        concepts.push(concept);
+        return concept;
+    }
 
-    parse_ce : function(t){
-        t = t.toLowerCase();
+    var add_instance = function(concept, name){
+        if(concepts.indexOf(concept) > -1){
+            instances.push({});
+        }
+    }
+
+    this.parse_ce = function(t){
+        t = t.trim();
         t = t.replace(/~/g, '');
         t = t.replace(/\ba\b/g, ''); 
         var facts = t.split(/\band\b/);
         var tokens = t.split(/[ ]+/);
-        console.log(tokens); 
         if(t.indexOf("conceptualise") == 0){
             var name = tokens[1];
             var parent = null;
@@ -58,7 +79,38 @@ var cestore = {
                     parent = tokens[i+2].trim();
                 }
             }
-            cestore.add_concept(name, parent);
+            return add_concept(name, parent);
+        }
+        if(t.indexOf("there is") == 0){
+            var concept = get_concept_by_name(tokens[2]);
+            if(concept == null){throw "Concept doesn't exist.";}
+            var instance = {};
+            instance.concept_id = concept.id;
+            for(var i = 0; i < tokens.length; i++){
+                for(var j = 0; j < relationships.length; j++){
+                    if(tokens[i] == relationships[j].ce[0]){
+                                  
+                    }
+                }
+            }
+                                    
         }
     }
-};
+
+    this.poll_cards = function(){
+        for(var i = 0; i < cards.length; i++){
+            
+        }
+    }
+
+    this.get_cards = function(){
+        return cards;
+    }    
+
+    this.receive_card = function(card_ce){
+        card_count ++;
+        card_ce = card_ce.replace('{timestamp}', "'"+new Date().getTime()+"'");
+        card_ce = card_ce.replace('{card_id}', "'msg_"+card_count+"'");
+        cards.push(card_ce);        
+    }
+}
