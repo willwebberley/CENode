fa9fb276a2da915b868ab1609c9e211bd84bcabd
Will Webberley <willwebberley@gmail.com>
2016-11-29 17:58:45 +0000

    updated gitignore

--- testing.js
+++
@@ -1,9 +0,0 @@
-const CENode = require('./CENode.js');
-const models = require('./models.js');
-
-const node = new CENode(models.CORE);
-node.add_sentence('there is an entity named \'harold\'');
-node.add_sentence("there is a tell card named 'df' that is to the agent 'Moira' and has 'conceptualise a ~ thingy ~ T that is an entity' as content and is from the agent 'macbook'")
-setTimeout(() => {
-  console.log(node.concepts.thingy)
-}, 1000)
