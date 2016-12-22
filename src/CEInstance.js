2f97b78c4a19bcf6491eb115cad6dd1cb1ba7285
Will Webberley <willwebberley@gmail.com>
2016-12-22 22:30:21 +0000

    CEInstance.js now conforms to AirBnb lint tests

--- src/CEInstance.js
+++ src/CEInstance.js
@@ -1,5 +1,3 @@
-
-
 class CEInstance {
 
   constructor(node, type, name, source) {
@@ -10,9 +8,9 @@ class CEInstance {
     this.node = node;
     this.typeId = type.id;
     this.sentences = [];
-    this._values = [];
-    this._relationships = [];
-    this._synonyms = [];
+    this.values = [];
+    this.relationships = [];
+    this.synonyms = [];
     this.reservedFields = ['values', 'relationships', 'synonyms', 'addValue', 'addRelationship', 'name', 'concept', 'id', 'instance', 'sentences', 'ce', 'gist'];
 
     const instance = this;
@@ -32,20 +30,21 @@ class CEInstance {
   }
 
   get type() {
-    for (let i = 0; i < this.node._concepts.length; i++) {
-      if (this.node._concepts[i].id == this.concept.id) {
-        return this.node._concepts[i];
+    for (let i = 0; i < this.node.concepts.length; i += 1) {
+      if (this.node.concepts[i].id === this.concept.id) {
+        return this.node.concepts[i];
       }
     }
+    return null;
   }
 
   get relationships() {
     const rels = [];
-    for (let i = 0; i < this._relationships.length; i++) {
+    for (let i = 0; i < this.relationships.length; i += 1) {
       const relationship = {};
-      relationship.label = this._relationships[i].label;
-      relationship.source = this._relationships[i].source;
-      relationship.instance = this.node.getInstanceById(this._relationships[i].targetId);
+      relationship.label = this.relationships[i].label;
+      relationship.source = this.relationships[i].source;
+      relationship.instance = this.node.getInstanceById(this.relationships[i].targetId);
       rels.push(relationship);
     }
     return rels;
@@ -53,14 +52,14 @@ class CEInstance {
 
   get values() {
     const vals = [];
-    for (let i = 0; i < this._values.length; i++) {
+    for (let i = 0; i < this.values.length; i += 1) {
       const value = {};
-      value.label = this._values[i].label;
-      value.source = this._values[i].source;
-      if (this._values[i].typeId == 0) {
-        value.instance = this._values[i].typeName;
+      value.label = this.values[i].label;
+      value.source = this.values[i].source;
+      if (this.values[i].typeId === 0) {
+        value.instance = this.values[i].typeName;
       } else {
-        value.instance = this.node.getInstanceById(this._values[i].typeId);
+        value.instance = this.node.getInstanceById(this.values[i].typeId);
       }
       vals.push(value);
     }
@@ -75,11 +74,11 @@ class CEInstance {
     const ancestorInstances = this.concept.ancestors;
     ancestorInstances.push(this.concept);
     const properties = { values: [], relationships: [] };
-    for (let i = 0; i < ancestorInstances.length; i++) {
-      for (let j = 0; j < ancestorInstances[i].values.length; j++) {
+    for (let i = 0; i < ancestorInstances.length; i += 1) {
+      for (let j = 0; j < ancestorInstances[i].values.length; j += 1) {
         properties.values.push(ancestorInstances[i].values[j].label.toLowerCase());
       }
-      for (let j = 0; j < ancestorInstances[i].relationships.length; j++) {
+      for (let j = 0; j < ancestorInstances[i].relationships.length; j += 1) {
         properties.relationships.push(ancestorInstances[i].relationships[j].label.toLowerCase());
       }
     }
@@ -93,24 +92,24 @@ class CEInstance {
       value.label = label;
       value.typeId = typeof valueInstance === 'object' ? valueInstance.id : 0;
       value.typeName = typeof valueInstance === 'object' ? valueInstance.name : valueInstance;
-      this._values.push(value);
+      this.values.push(value);
       const valueNameField = label.toLowerCase().replace(/ /g, '_');
 
-      if (this.reservedFields.indexOf(valueNameField) == -1) {
+      if (this.reservedFields.indexOf(valueNameField) === -1) {
         Object.defineProperty(this, valueNameField, {
           get() {
-            return value.typeId == 0 ? value.typeName : this.node.getInstanceById(value.typeId);
+            return value.typeId === 0 ? value.typeName : this.node.getInstanceById(value.typeId);
           },
           configurable: true,
         });
 
-        if (this.reservedFields.indexOf(`${valueNameField}s`) == -1 && !this.hasOwnProperty(`${valueNameField}s`)) {
+        if (this.reservedFields.indexOf(`${valueNameField}s`) === -1 && !Object.prototype.hasOwnProperty.call(this, `${valueNameField}s`)) {
           Object.defineProperty(this, `${valueNameField}s`, {
             get() {
               const instances = [];
-              for (let i = 0; i < this._values.length; i++) {
-                if (this._values[i].label.toLowerCase().replace(/ /g, '_') == valueNameField) {
-                  instances.push(this._values[i].typeId == 0 ? this._values[i].typeName : this.node.getInstanceById(this._values[i].typeId));
+              for (let i = 0; i < this.values.length; i += 1) {
+                if (this.values[i].label.toLowerCase().replace(/ /g, '_') === valueNameField) {
+                  instances.push(this.values[i].typeId === 0 ? this.values[i].typeName : this.node.getInstanceById(this.values[i].typeId));
                 }
               }
               return instances;
@@ -118,7 +117,7 @@ class CEInstance {
           });
         }
       }
-      if (propagate == null || propagate != false) {
+      if (propagate === null || propagate !== false) {
         this.node.enactRules(this, 'value', valueInstance, source);
       }
     }
@@ -131,10 +130,10 @@ class CEInstance {
       relationship.source = source;
       relationship.targetId = relationshipInstance.id;
       relationship.targetName = relationshipInstance.name;
-      this._relationships.push(relationship);
+      this.relationships.push(relationship);
       const relNameField = label.toLowerCase().replace(/ /g, '_');
 
-      if (this.reservedFields.indexOf(relNameField) == -1) {
+      if (this.reservedFields.indexOf(relNameField) === -1) {
         Object.defineProperty(this, relNameField, {
           get() {
             return this.node.getInstanceById(relationship.targetId);
@@ -142,13 +141,13 @@ class CEInstance {
           configurable: true,
         });
 
-        if (this.reservedFields.indexOf(`${relNameField}s`) == -1 && !this.hasOwnProperty(`${relNameField}s`)) {
+        if (this.reservedFields.indexOf(`${relNameField}s`) === -1 && !Object.prototype.hasOwnProperty.call(this, `${relNameField}s`)) {
           Object.defineProperty(this, `${relNameField}s`, {
             get() {
               const instances = [];
-              for (let i = 0; i < this._relationships.length; i++) {
-                if (this._relationships[i].label.toLowerCase().replace(/ /g, '_') == relNameField) {
-                  instances.push(this.node.getInstanceById(this._relationships[i].targetId));
+              for (let i = 0; i < this.relationships.length; i += 1) {
+                if (this.relationships[i].label.toLowerCase().replace(/ /g, '_') === relNameField) {
+                  instances.push(this.node.getInstanceById(this.relationships[i].targetId));
                 }
               }
               return instances;
@@ -156,42 +155,42 @@ class CEInstance {
           });
         }
       }
-      if (propagate == null || propagate != false) {
+      if (propagate === null || propagate !== false) {
         this.node.enactRules(this, 'relationship', relationshipInstance, source);
       }
     }
   }
 
   addSynonym(synonym) {
-    for (let i = 0; i < this._synonyms.length; i++) {
-      if (this._synonyms[i].toLowerCase() == synonym.toLowerCase()) {
+    for (let i = 0; i < this.synonyms.length; i += 1) {
+      if (this.synonyms[i].toLowerCase() === synonym.toLowerCase()) {
         return;
       }
     }
-    this._synonyms.push(synonym);
+    this.synonyms.push(synonym);
     Object.defineProperty(this, synonym.toLowerCase().replace(/ /g, '_'), {
       get() {
-        return instance;
+        return this;
       },
     });
   }
 
-  property(propertyNamee, source) {
+  property(propertyName, source) {
     return this.properties(propertyName, source, true);
   }
 
   properties(propertyName, source, onlyOne) {
     const properties = [];
-    for (let i = this.values.length - 1; i >= 0; i--) { // Reverse so we get the latest prop first
-      if (this.values[i].label.toLowerCase() == propertyName.toLowerCase()) {
+    for (let i = this.values.length - 1; i >= 0; i -= 1) { // Reverse so we get the latest prop first
+      if (this.values[i].label.toLowerCase() === propertyName.toLowerCase()) {
         const inst = this.values[i].instance;
         const dat = source ? { instance: inst, source: this.values[i].source } : inst;
         if (onlyOne) { return dat; }
         properties.push(dat);
       }
     }
-    for (let i = this.relationships.length - 1; i >= 0; i--) { // Reverse so we get the latest prop first
-      if (this.relationships[i].label.toLowerCase() == propertyName.toLowerCase()) {
+    for (let i = this.relationships.length - 1; i >= 0; i -= 1) { // Reverse so we get the latest prop first
+      if (this.relationships[i].label.toLowerCase() === propertyName.toLowerCase()) {
         const inst = this.relationships[i].instance;
         const dat = source ? { instance: inst, source: this.relationships[i].source } : inst;
         if (onlyOne) { return dat; }
@@ -202,17 +201,17 @@ class CEInstance {
   }
 
   get synonyms() {
-    return this._synonyms;
+    return this.synonyms;
   }
 
   get ce() {
     const concept = this.concept;
-    if (concept == null) { return; }
+    if (concept === null) { return ''; }
     let ce = `there is a ${concept.name} named '${this.name}'`;
     const facts = [];
-    for (let i = 0; i < this._values.length; i++) {
-      const value = this._values[i];
-      if (value.typeId == 0) {
+    for (let i = 0; i < this.values.length; i += 1) {
+      const value = this.values[i];
+      if (value.typeId === 0) {
         facts.push(`has '${value.typeName.replace(/'/g, "\\'")}' as ${value.label}`);
       } else {
         const valueInstance = this.node.getInstanceById(value.typeId);
@@ -220,8 +219,8 @@ class CEInstance {
         facts.push(`has the ${valueConcept.name} '${valueInstance.name}' as ${value.label}`);
       }
     }
-    for (let i = 0; i < this._relationships.length; i++) {
-      const relationship = this._relationships[i];
+    for (let i = 0; i < this.relationships.length; i += 1) {
+      const relationship = this.relationships[i];
       const relationshipInstance = this.node.getInstanceById(relationship.targetId);
       const relationshipConcept = relationshipInstance.type;
       facts.push(`${relationship.label} the ${relationshipConcept.name} '${relationshipInstance.name}'`);
@@ -233,16 +232,16 @@ class CEInstance {
   get gist() {
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     const concept = this.concept;
-    if (concept == null) { return; }
+    if (concept === null) { return ''; }
     let gist = `${this.name} is`;
     if (vowels.indexOf(concept.name.toLowerCase()[0]) > -1) { gist += ` an ${concept.name}.`; } else { gist += ` a ${concept.name}.`; }
     const facts = {};
     let factFound = false;
-    for (let i = 0; i < this._values.length; i++) {
+    for (let i = 0; i < this.values.length; i += 1) {
       factFound = true;
-      const value = this._values[i];
+      const value = this.values[i];
       let fact = '';
-      if (value.typeId == 0) {
+      if (value.typeId === 0) {
         fact = `has '${value.typeName.replace(/'/g, "\\'")}' as ${value.label}`;
       } else {
         const valueInstance = this.node.getInstanceById(value.typeId);
@@ -252,22 +251,22 @@ class CEInstance {
       if (!(fact in facts)) {
         facts[fact] = 0;
       }
-      facts[fact]++;
+      facts[fact] += 1;
     }
-    for (let i = 0; i < this._relationships.length; i++) {
+    for (let i = 0; i < this.relationships.length; i += 1) {
       factFound = true;
-      const relationship = this._relationships[i];
+      const relationship = this.relationships[i];
       const relationshipInstance = this.node.getInstanceById(relationship.targetId);
       const relationshipConcept = relationshipInstance.type;
       const fact = `${relationship.label} the ${relationshipConcept.name} '${relationshipInstance.name}'`;
       if (!(fact in facts)) {
         facts[fact] = 0;
       }
-      facts[fact]++;
+      facts[fact] += 1;
     }
     if (factFound) {
       gist += ` ${this.name}`;
-      for (const fact in facts) {
+      for (const fact of facts) {
         gist += ` ${fact}`;
         if (facts[fact] > 1) {
           gist += ` (${facts[fact]} times)`;
