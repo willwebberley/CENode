8976b9544c19d524c6dac7b634ed45f9202025a5
Will Webberley <willwebberley@gmail.com>
2017-01-21 11:28:07 +0000

    tidied up policyHandler class to remove uneccessary try/catches

--- src/PolicyHandler.js
+++ src/PolicyHandler.js
@@ -75,35 +75,26 @@ class PolicyHandler {
       'tell policy': (policy) => {
         // For each tell policy in place, send all currently-untold cards to each target
         // multiple cards to be sent to one target line-separated
-        const target = policy.target;
-        if (target && target.name) {
-          const cards = this.unsentTellCards[target.name];
-          if (cards) {
-            let data = '';
-            for (let j = 0; j < cards.length; j += 1) {
-              try {
-                const card = cards[j];
-                const from = card.is_from;
-                const tos = card.is_tos;
-                if (tos && from.name.toLowerCase() !== target.name.toLowerCase()) { // Don't send back a card sent from target agent
-                  // Make sure target is not already a recipient
-                  let inCard = false;
-                  for (let k = 0; k < tos.length; k += 1) {
-                    if (tos[k].id === target.id) { inCard = true; break; }
-                  }
-                  if (!inCard) {
-                    card.addRelationship('is to', target);
-                  }
-                  data += `${card.ce}\n`;
-                }
-              } catch (err) { /* Continue anyway */ }
+        if (policy.target && policy.target.name) {
+          let data = '';
+          for (const card of this.unsentTellCards[target.name]) {
+            if (card.is_tos && card.is_from.name.toLowerCase() !== target.name.toLowerCase()) { // Don't send back a card sent from target agent
+              // Make sure target is not already a recipient
+              let inCard = false;
+              for (const to of card.is_tos) {
+                if (to.id === target.id) { inCard = true; break; }
+              }
+              if (!inCard) {
+                card.addRelationship('is to', target);
+              }
+              data += `${card.ce}\n`;
             }
-            if (data !== '') {
-              net.makeRequest('POST', target.address, POST_SENTENCES_ENDPOINT, data, () => {
-                this.lastSuccessfulRequest = new Date().getTime();
-                this.unsentTellCards[target.name] = [];
-              });
-            }
+          }
+          if (data.length) {
+            net.makeRequest('POST', target.address, POST_SENTENCES_ENDPOINT, data, () => {
+              this.lastSuccessfulRequest = new Date().getTime();
+              this.unsentTellCards[target.name] = [];
+            });
           }
         }
       },
@@ -111,60 +102,50 @@ class PolicyHandler {
       'ask policy': (policy) => {
         // For each ask policy in place send all currently-untold cards to each target
         // multiple cards to be sent to one target are line-separated
-        const target = policy.target;
-        if (target && target.name) {
-          const cards = this.unsentAskCards[target.name];
-          if (cards) {
-            let data = '';
-            for (let j = 0; j < cards.length; j += 1) {
-              try {
-                const card = cards[j];
-                const from = card.is_from;
-                const froms = card.is_froms;
-                const tos = card.is_tos;
-                if (tos && from && from.name.toLowerCase() !== target.name.toLowerCase()) { // Don't send back a card sent from target agent
-                  // Make sure target is not already a recipient
-                  let inCard = false;
-                  for (let k = 0; k < tos.length; k += 1) {
-                    if (tos[k].id === target.id) { inCard = true; break; }
-                  }
-                  if (!inCard) {
-                    card.addRelationship('is to', target);
-                  }
-                  // Make sure an agent is not already a sender
-                  inCard = false;
-                  for (let k = 0; k < froms.length; k += 1) {
-                    if (froms[k].id === this.agent.getInstance().id) { inCard = true; break; }
-                  }
-                  if (!inCard) {
-                    card.addRelationship('is from', this.agent.getInstance());
-                  }
-                  data += `${card.ce}\n`;
-                }
-              } catch (err) { /* Continue anyway */ }
+        if (policy.target && policy.target.name) {
+          let data = '';
+          for (const card of this.unsentAskCards[policy.target.name]) {
+            const froms = card.is_froms;
+            const tos = card.is_tos;
+            if (tos && card.is_from && card.is_from.name.toLowerCase() !== policy.target.name.toLowerCase()) { // Don't send back a card sent from target agent
+              // Make sure target is not already a recipient
+              let inCard = false;
+              for (const to of tos) {
+                if (to.id === policy.target.id) { inCard = true; break; }
+              }
+              if (!inCard) {
+                card.addRelationship('is to', policy.target);
+              }
+              // Make sure an agent is not already a sender
+              inCard = false;
+              for (const from of froms) {
+                if (from.id === this.agent.getInstance().id) { inCard = true; break; }
+              }
+              if (!inCard) {
+                card.addRelationship('is from', this.agent.getInstance());
+              }
+              data += `${card.ce}\n`;
             }
-            if (data !== '') {
-              net.makeRequest('POST', target.address, POST_SENTENCES_ENDPOINT, data, () => {
-                this.lastSuccessfulRequest = new Date().getTime();
-                this.unsentAskCards[target.name] = [];
-              });
-            }
+          }
+          if (data.length) {
+            net.makeRequest('POST', target.address, POST_SENTENCES_ENDPOINT, data, () => {
+              this.lastSuccessfulRequest = new Date().getTime();
+              this.unsentAskCards[target.name] = [];
+            });
           }
         }
       },
 
       'listen policy': (policy) => {
-        // For each listen policy in place, make a request to get cards addressed to THIS agent, and add to node, ignoring already-seen cards
-        const target = policy.target;
+        // Make request to target to get cards addressed to THIS agent
+        // Build ignore list of already-processed cards:
         let data = '';
-        const allCards = this.node.getInstances('card', true);
-        for (let j = 0; j < allCards.length; j += 1) {
-          data = `${data + allCards[j].name}\n`;
+        for (const card of this.node.getInstances('card', true)) {
+          data = `${data + card.name}\n`;
         }
-        net.makeRequest('POST', target.address, `${GET_CARDS_ENDPOINT}?agent=${this.agent.name}`, data, (resp) => {
+        net.makeRequest('POST', policy.target.address, `${GET_CARDS_ENDPOINT}?agent=${this.agent.name}`, data, (res) => {
           this.lastSuccessfulRequest = new Date().getTime();
-          const cards = resp.split('\n');
-          this.node.addSentences(cards);
+          this.node.addSentences(res.split('\n'));
         });
       },
 
