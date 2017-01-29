1f9ce962fb480e0c2928ec954de3b919f9489406
Will Webberley <willwebberley@gmail.com>
2017-01-29 18:49:41 +0000

    created new handlers object for HTTP requests

--- src/CEServer.js
+++ src/CEServer.js
@@ -22,139 +22,128 @@ const CEModels = require('../models/index.js');
 
 const POST_SENTENCES_ENDPOINT = '/sentences';
 const GET_CARDS_ENDPOINT = '/cards';
+let server;
 
-function postSentences(node, request, response) {
-  let body = '';
-  request.on('data', (chunk) => { body += chunk; });
-  request.on('end', () => {
-    body = decodeURIComponent(body.replace('sentence=', '').replace(/\+/g, ' '));
-    const sentences = body.split(/\\n|\n/);
-    const responses = node.addSentences(sentences);
-    response.write(responses.map(resp => resp.data).join('\n'));
-    response.end();
-  });
-}
-
-function getCards(node, request, response, ignoresInput) {
-  const url = decodeURIComponent(request.url);
-  const agentRegex = url.match(/agent=(.*)/);
-  const ignores = ignoresInput || [];
-  let agentStr = null;
-  let agents = [];
-  if (agentRegex) { agentStr = agentRegex[1]; }
-  if (agentStr) {
-    agents = agentStr.toLowerCase().split(',');
-  }
-  const cards = node.getInstances('card', true);
-  let s = '';
-  for (let i = 0; i < cards.length; i += 1) {
-    if (ignores.indexOf(cards[i].name) === -1) {
-      if (!agents || agents.length === 0) {
-        s += `${cards[i].ce}\n`;
-      } else {
-        const tos = cards[i].is_tos;
-        if (tos) {
-          for (let j = 0; j < tos.length; j += 1) {
-            for (let k = 0; k < agents.length; k += 1) {
-              if (tos[j].name.toLowerCase() === agents[k]) {
-                s += `${cards[i].ce}\n`;
-                break;
-              }
+const handlers = {
+  'GET': {
+    '/sentences': (node, request, response) => {
+      response.writeHead(200, { 'Content-Type': 'text/ce' });
+      const agentRegex = decodeURIComponent(request.url).match(/agent=(.*)/);
+      const agentStr = agentRegex ? agentRegex[1] : null;
+      const agents = (agentStr && agentStr.toLowerCase().split(',')) || [];
+      let s = '';
+      for (const card of node.getInstances('card', true)) {
+        for (const to of card.is_tos) {
+          for (const agent of agents) {
+            if (to.name.toLowerCase() === agent) {
+              s += `${card.ce}\n`;
+              break;
             }
           }
         }
       }
+      response.write(s);
+      response.end();
     }
-  }
-  response.write(s);
-  response.end();
-}
-
-function startServer() {
-  const node = new CENode(CEModels.core, CEModels.server);
-  node.attachAgent();
-  let port = 5555;
-  if (process.argv.length > 3) {
-    port = process.argv[3];
-  }
-  if (process.argv.length > 2) {
-    node.agent.setName(process.argv[2]);
-  }
-
-  http.createServer((request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-
-    if (request.method === 'GET') {
-      if (request.url === '/') {
-        const ins = node.instances;
-        const con = node.concepts;
-        let s = '<html><head><title>CENode Management</title></head><body><h1>CENode Server Admin Interface</h1>';
-        s += '<div style="width:48%;float:left;"><h2>Conceptual model</h2>';
-        s += '<p>Add CE sentences to the node:</p><form action="/ui/sentences" enctype="application/x-www-form-urlencoded" method="POST"><textarea name="sentence" style="width:95%;height:100px;"></textarea><br /><br /><input type="submit" /></form></div>';
-        s += `<div style="width:48%;float:left;"><h2>Node settings</h2><p>Update local agent name:</p><form method="POST" action="/agent-name"><input type="text" name="name" value="${node.agent.name}" /><input type="submit" /></form>`;
-        s += '<p>Other options:</p><button onclick="window.location=\'/reset\';">Empty model</button>';
-        s += '</div><div style="clear:both;"></div>';
-        s += '<div style="display:inline-block;width:45%;float:left;"><h2>Concepts</h2>';
-        for (const concept of con) {
-          s += concept.name;
-          if (concept.parents.length) {
-            s += ` (${concept.parents[0].name})`;
-          }
-          s += '<br>';
-        }
-        s += '</div><div style="display:inline-block;width:45%;float:right;"><h2>Instances</h2>';
-        for (const instance of ins) {
-          s += `${instance.name} (${instance.type.name})<br>`;
-        }
-        s += '</div><body></html>';
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(s);
-      } else if (request.url.indexOf(GET_CARDS_ENDPOINT) === 0) {
+  },
+  'POST': {
+    '/cards': (node, request, response) => {
+      let body = '';
+      request.on('data', (chunk) => { body += chunk; });
+      request.on('end', () => {
         response.writeHead(200, { 'Content-Type': 'text/ce' });
-        getCards(node, request, response);
-      } else if (request.url === '/reset') {
-        node.resetAll();
+        const ignores = body.split(/\\n|\n/);
+        const agentRegex = decodeURIComponent(request.url).match(/agent=(.*)/);
+        const agentStr = agentRegex ? agentRegex[1] : null;
+        const agents = (agentStr && agentStr.toLowerCase().split(',')) || [];
+        let s = '';
+        for (const card of node.getInstances('card', true)) {
+          if (ignores.indexOf(card.name) === -1) {
+            if (agents.length === 0) {
+              s += `${card.ce}\n`;
+            } else {
+              for (const to of card.is_tos) {
+                for (const agent of agents) {
+                  if (to.name.toLowerCase() === agent) {
+                    s += `${card.ce}\n`;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+        }
+        response.write(s);
+        response.end();
+      });
+    },
+    '/sentences': (node, request, response) => {
+      response.writeHead(200, { 'Content-Type': 'text/ce' });
+      let body = '';
+      request.on('data', (chunk) => { body += chunk; });
+      request.on('end', () => {
+        body = decodeURIComponent(body.replace('sentence=', '').replace(/\+/g, ' '));
+        const sentences = body.split(/\\n|\n/);
+        const responses = node.addSentences(sentences);
+        response.write(responses.map(resp => resp.data).join('\n'));
+        response.end();
+      });
+    }
+  },
+  'PUT': {
+    '/reset': (node) => {
+      node.resetAll();
+      response.writeHead(204);
+      response.end();
+    },
+    '/agent/name': (node, request, response) => {
+      let body = '';
+      request.on('data', (chunk) => { body += chunk; });
+      request.on('end', () => {
+        body = decodeURIComponent(body.replace('name=', '').replace(/\+/g, ' '));
+        node.agent.setName(body);
         response.writeHead(302, { Location: '/' });
         response.end();
-      } else {
-        response.writeHead(404);
-        response.end('404: Resource not found for method GET.');
+      });
+    }
+  }
+};
+
+function startServer(name, port) {
+  const node = new CENode(CEModels.core, CEModels.server);
+  node.attachAgent();
+  node.agent.setName(name);
+
+  server = http.createServer((request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    if (request.method in handlers) {
+      const path = request.url.indexOf('?') > 1 ? request.url.slice(0, request.url.indexOf('?')) : request.url;
+      if (path in handlers[request.method]) {
+        handlers[request.method][path](node, request, response);
       }
-    } else if (request.method === 'POST') {
-      if (request.url.indexOf(GET_CARDS_ENDPOINT) === 0) {
-        let body = '';
-        request.on('data', (chunk) => { body += chunk; });
-        request.on('end', () => {
-          const ignores = body.split(/\\n|\n/);
-          response.writeHead(200, { 'Content-Type': 'text/ce' });
-          getCards(node, request, response, ignores);
-        });
-      } else if (request.url === POST_SENTENCES_ENDPOINT) {
-        response.writeHead(200, { 'Content-Type': 'text/ce' });
-        postSentences(node, request, response);
-      } else if (request.url === '/ui/sentences') {
-        response.writeHead(302, { Location: '/' });
-        postSentences(node, request, response);
-      } else if (request.url === '/agent-name') {
-        let body = '';
-        request.on('data', (chunk) => { body += chunk; });
-        request.on('end', () => {
-          body = decodeURIComponent(body.replace('name=', '').replace(/\+/g, ' '));
-          node.agent.setName(body);
-          response.writeHead(302, { Location: '/' });
-          response.end();
-        });
-      } else {
+      else {
         response.writeHead(404);
-        response.end('404: Resource not found for method POST.');
+        response.end(`404: Resource not found for method ${request.method}.`);
       }
-    } else {
+    }
+    else {
       response.writeHead(405);
       response.end('405: Method not allowed on this server.');
     }
-  }).listen(port || 5555);
+  });
+  server.listen(port);
+}
+
+function stopServer (){
+  if (server) {
+    server.close();
+  }
 }
 
 if (require.main === module) {
-  startServer();
+  const name = process.argv.length > 2 ? process.argv[2] : 'Moira';
+  const port = process.argv.length > 3 ? process.argv[3] : 5555;
+  startServer(name, port);
 }
+
+module.exports = {startServer, stopServer};
